41a7e854c9c45e8220e8c7f649024be70dc28c81
cherry324 <cherry324@missionrnd.com>
2016-03-30 17:11:15 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -12,12 +12,32 @@ NOTES:
 */
 
 #include <stdio.h>
+#include<malloc.h>
 
 struct node {
 	int num;
 	struct node *next;
 };
 
-struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+
+struct node * merge2LinkedLists(struct node *head1, struct node *head2)
+{
+	struct node *result = NULL;
+	if (head1 == NULL&&head2 == NULL)return NULL;/*invalid input*/
+	if (head1 == NULL)
+		return head2;
+	if (head2 == NULL)
+		return head1;
+	
+	if (head1->num<head2->num)/*comparing two numbers from two linked list*/
+	{
+		result = head1;
+		result->next = merge2LinkedLists(head1->next, head2);
+	}
+	else
+	{
+		result = head2;
+		result->next = merge2LinkedLists(head1, head2->next);
+	}
+	return result;
 }
